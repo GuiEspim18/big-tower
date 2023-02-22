d498f89b0b100575215f3c2aaf3d982f69dbe706
GuiEspim18 <guilherme.monteiro.espim@gmail.com>
2023-02-21 23:29:22 -0300

    trying to resolve the route problem

--- src/app/shared/utils/variables/carousel-images/carousel-images.ts
+++ src/app/shared/utils/variables/carousel-images/carousel-images.ts
@@ -2,13 +2,13 @@ import { CarouselImages } from "./dto/carousel-images.dto";
 
 export const carouselImages: Array<CarouselImages> = [
     {
-        path: '../../../../assets/img/carousel/carousel1.jpg'
+        path: 'assets/img/carousel/carousel1.jpg'
     },
     {
-        path: '../../../../assets/img/carousel/carousel2.jpg'
+        path: 'assets/img/carousel/carousel2.jpg'
     },
     {
-        path: '../../../../assets/img/carousel/carousel3.JPG'
+        path: 'assets/img/carousel/carousel3.JPG'
     }
     
 ];

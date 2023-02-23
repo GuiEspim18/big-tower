7862c9db2bcebc718326045a841f648498ccd467
GuiEspim18 <guilherme.monteiro.espim@gmail.com>
2023-02-22 21:13:12 -0300

    menu bug

--- src/app/shared/utils/side-nav-bar/side-nav-bar.component.ts
+++ src/app/shared/utils/side-nav-bar/side-nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Animations } from '../animations/animations';
 import { Pages } from '../variables/pages/dto/pages.dto';
 import { pages } from '../variables/pages/pages';
@@ -21,11 +21,23 @@ export class SideNavBarComponent implements OnInit {
 
   public readonly myColor: string = "#00000066"
 
+  @Output() private readonly close: EventEmitter<any> = new EventEmitter()
+
 
 
   constructor() { }
 
+
+  /* On Init */
+
   ngOnInit(): void {
+  }
+
+
+  /* Emit event */
+
+  public emit(): void {
+    this.close.emit(true)
   }
 
 }
